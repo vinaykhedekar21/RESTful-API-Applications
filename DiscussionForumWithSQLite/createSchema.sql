6a54484b3997b352946d8cf4dbd388bc47602293
samarth-29 <32147591+samarth-29@users.noreply.github.com>
2018-10-29 01:21:37 -0700

    Create createSchema.sql

---
+++ DiscussionForumWithSQLite/createSchema.sql
@@ -0,0 +1,22 @@
+drop table if exists user;
+create table user (
+  user_id integer primary key autoincrement,
+  username text not null,
+  password text not null
+);
+
+drop table if exists forum;
+create table forum (
+  forum_id integer primary key autoincrement,
+  name text not null,
+  user_id integer not null
+);
+
+drop table if exists thread;
+create table thread (
+  thread_id GUID primary key,
+  forum_id integer not null,
+  title text not null
+);
+
+
